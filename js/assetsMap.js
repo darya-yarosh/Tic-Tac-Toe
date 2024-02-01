653ddfdec9248bbcba2e4d131321c461fcef4ce2
darya-yarosh <gurimusagi@yandex.ru>
2024-02-01 12:49:30 +0300

    Updated assets.

--- js/assetsMap.js
+++ js/assetsMap.js
@@ -6,6 +6,10 @@ const assetsMap = {
         { name: "CellDefault", url: '../assets/CellDefault.png' },
         { name: "CellDown", url: '../assets/CellDown.png' },
         { name: "CellOver", url: '../assets/CellOver.png' },
+        { name: "CellOverSelected", url: '../assets/CellOverSelected.png' },
+        { name: "InputDefault", url: '../assets/InputDefault.png' },
+        { name: "InputDown", url: '../assets/InputDown.png' },
+        { name: "InputOver", url: '../assets/InputOver.png' },
     ],
     fonts: [
         { name: "ComicoroRu", url: "../assets/ComicoroRu.ttf"},
